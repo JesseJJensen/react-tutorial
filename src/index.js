9b34a23a1df70a179c93c6a8e6d16e04450bdda7
Jesse Jensen <81991370+JesseJJensen@users.noreply.github.com>
2021-05-28 16:40:36 -0400

    added comple btn; onClick w/ arrow function

--- src/index.js
+++ src/index.js
@@ -36,19 +36,26 @@ function BookList() {
   )
 }
 
-const Book = ({ img, title, author }) => {
+const Book = (props) => {
+  const { img, title, author } = props
   // attribute, eventHandler
   //onClick, onMouseOver
   const clickHandler = () => {
     alert('hello world')
   }
+  const complexExample = (author) => {
+    console.log(author);
+  }
   return (
     <article className='book'>
       <img src={img} alt='' />
-      <h1 onClick={()=> console.log(title)}>{title}</h1>
+      <h1 onClick={() => console.log(title)}>{title}</h1>
       <h4>{author}</h4>
       <button type='button' onClick={clickHandler}>
         reference example
+      </button>
+      <button type='button' onClick={() => complexExample(author)}>
+        complex example
       </button>
     </article>
   )
