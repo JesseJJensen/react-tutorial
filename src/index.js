1b699e4b9fa625ce1f83bf4e34339deb5660da93
Jesse Jensen <81991370+JesseJJensen@users.noreply.github.com>
2021-05-28 08:17:15 -0400

    added props

--- src/index.js
+++ src/index.js
@@ -12,24 +12,24 @@ const img = 'https://m.media-amazon.com/images/I/81AQ8xjnNPL._AC_UY218_.jpg'
 function BookList() {
   return (
     <section className='booklist'>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book job='developer' />
+      <Book title='random title' number={22}/>
     </section>
   )
 }
 
-const Book = () => {
+const Book = (props) => {
+  console.log(props)
   return (
     <article className='book'>
       <img src={img} alt='' />
       <h1>{title}</h1>
       <h4>{author.toUpperCase()}</h4>
-      <p>{6 + 6}</p>
+      <p>{props.job}</p>
+      <p>{props.title}</p>
+      <p>{props.number}</p>
     </article>
   )
 }
-
 
 ReactDom.render(<BookList />, document.getElementById('root'))
